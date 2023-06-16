04d1637b0efd2d02404b43c11256119a6aae1d7e
rutika <rutikashinde76@gmail.com>
2023-06-16 20:58:33 +0530

    CRUD and Session Functions

--- dnlib/class/database.class.php
+++ dnlib/class/database.class.php
@@ -24,13 +24,53 @@ class Database{
         } 
         return $dt;
       }
+      private function getLabels($values){
+        $label="";
+        foreach($values as $value){
+            $label .='?,';
+        }
+        $label = substr_replace($label ,'',-1);
+        return $label;
+      }
+      //for update method
+      private function getLabelsWithName($columns){
+        $label="";
+        $columns = explode(',',$columns);
 
+        foreach($columns as $column){
+            $label .=$column.'=?,';
+        }
+        $label = substr_replace($label ,'',-1);
+        return $label;
+      }
+
+      //dynamic function to insert data 
       public function insert($table, $columns, $values){
-            die($this->getBindParmsDataType($values));
+        $label = $this->getLabels($values);
         $query = "INSERT INTO $table($columns) VALUES(?,?)";
         $obj=$this->connection->prepare($query);
-        $obj-> bind_param('si',...$values);
-        $obj->execute();
+        $obj-> bind_param($this->getBindParmsDataType($values),...$values);
+        return $obj->execute();
+      }
+      
+      public function read($table, $columns="*", $conditions=''){
+        $query = "SELECT $columns FROM $table $conditions";
+         $result = $this->connection->query($query);
+         return $result->fetch_all(true);
+
+      }
+       
+      public function delete($table,$condition){
+        $query = "DELETE FROM $table WHERE $condition";
+        return $this->connection->query($query);
+      }
+      public function update($table, $columns, $values, $condition){
+        $label = $this->getLabelsWithName($columns);
+        $query = "UPDATE $table SET $label WHERE $condition";
+        //die($query);
+        $obj=$this->connection->prepare($query);
+        $obj-> bind_param($this->getBindParmsDataType($values),...$values);
+        return $obj->execute();
       }
 
 }
--- dnlib/class/session.class.php
+++ dnlib/class/session.class.php
@@ -2,4 +2,18 @@
 
 class Session{
     
+    public function set($key, $value){
+        $_SESSION[$key]= $value;
+    }
+    public function get($key){
+        return $_SESSION[$key]??'';
+    }
+    public function delete($key){
+        unset($_SESSION[$key]);
+    }
+    
+    public function destroy($key){
+        session_destroy();
+    }
+
 }
