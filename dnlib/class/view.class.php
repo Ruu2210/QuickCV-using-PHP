6b8410815a82715fc3254cc435194dfcd55981f1
rutika <rutikashinde76@gmail.com>
2023-06-17 12:26:11 +0530

    done with routing part

--- dnlib/class/view.class.php
+++ dnlib/class/view.class.php
@@ -1,5 +1,12 @@
 <?php
 
 class View{
-    
+    public function load($page, $data=array()){
+        global $action;
+        
+        extract($data,EXTR_SKIP);
+          
+        include("page/$page.php");
+
+    }
 }
